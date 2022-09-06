eb4d9b420364a6a8331e4e4c8a1f29b97bc33a79
caukien <backien2k2@gmail.com>
2022-09-06 08:43:16 +0700

    bai 5

---
+++ App.js
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={hinh.shape1}>
+        <Text>hinh 1</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={hinh.shape2}>
+        <Text>hinh 2</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={hinh.shape3}>
+        <Text>hinh 3</Text>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1  ,
+    // backgroundColor: '#FFFFF',
+    alignItems: 'center',
+    margin: 50,
+    flexDirection: 'row',
+    justifyContent:'space-around',
+  },
+});
+
+const hinh= StyleSheet.create({
+  shape1: {
+    width: 100,
+    height: 100,
+    backgroundColor : '#00FFFF',
+    color: 'white',
+    alignItems: 'center',
+    // justifyContent: 'space-around',
+ },
+ shape2: {
+  width: 100,
+  height: 100,
+  backgroundColor : '#00CC66',
+  color: 'white',
+  alignItems: 'center',
+  // justifyContent: 'center',
+  },
+shape3: {
+  width: 100,
+  height: 100,
+  backgroundColor : '#DD0000',
+  color: 'white',
+  alignItems: 'center',
+  // justifyContent: 'center',
+  },
+});
